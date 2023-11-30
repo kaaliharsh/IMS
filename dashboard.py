e2adc282aa9fe14f59b22b477d13b8e4a0f6cc3d
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2023-11-30 14:38:03 +0530

    new commit

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,21 @@
-import tkinter as tk
+from tkinter import*
+from PIL import Image,ImageTk         #for image view
 class IMS:
     def __init__(self,root):
         self.root=root
         self.root.geometry("1350x700+0+0")
         self.root.title("Inventory Management System Developed by Team Null")
         
-        #--------Title----------------
+        #--------Title----------------------------------
+        self.icon_title=PhotoImage(file="images/logo_ims.png")
         
-        title=Label(self.root,text="Inventory Management System",font=("times new roman",40,"bild")).place(x=0,y=0,relwidth=1,height=70)
-        root=Tk()
-        obj=IMS(root)
-        root.mainloop()
+        title=Label(self.root,text="Inventory Management System",image=self.icon_title,compound=LEFT,font=("times new roman",40,"bold"),bg="#010c48",fg="white",padx=20,anchor="w").place(x=0,y=0,relwidth=1,height=70)
+        
+        
+        #----------logout button-------------------------
+        btn_logout=Button(self.root,text="Log out", font=("times new roman"))
+        
+        
+root=Tk()
+obj=IMS(root)
+root.mainloop() 
